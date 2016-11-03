0be6601c25d8c1b01001ee4aa345adde8cc72830
yuichiroTCY <t.yic.yt@gmail.com>
2016-11-03 17:00:44 +0900

    Fix js

--- static/js/jquery-func.js
+++ static/js/jquery-func.js
@@ -1,18 +1,6 @@
-!function ($) {
-	//=================================== scroll  ===================================//
-
-$body.scrollspy({
-      target: '#navbar-main',
-      offset: navHeight
-    })
-
-    $window.on('load', function () {
-      $body.scrollspy('refresh')
-    })
-
-    $('#navbar-main [href=#]').click(function (e) {
-      e.preventDefault()
-    })
-
-
+;$(document).ready(function() {
+  $('body').scrollspy({
+    target: '#navbar-main',
+    offset: 50
+  });
 });
